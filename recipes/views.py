cf103f072645fe387c8a898f6a901679bcca3c72
Augusto Fadanelli de Sousa <augustofadanelli@protonmail.com>
2022-08-01 11:40:07 -0300

    Reorganizado diretórios em recipes/templates. Add styles. Criado header do site.

--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,5 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Augusto Fadanelli'})
+    return render(request, 'recipes/pages/home.html')
 
